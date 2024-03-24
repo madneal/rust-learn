9d5b7bde9bc79a56094a96c621e4acefae66cd1d
madneal <bing.ecnu@gmail.com>
2024-03-24 10:28:25 +0800

    add root template file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use rocket::{get, launch, routes};
+use rocket::fs::{FileServer, Options, relative};
+use rocket_dyn_templates::Template;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(Template::fairing())
+        .mount("/public", FileServer::new(relative!("/public"), Options::Missing | Options::NormalizeDirs))
         .mount("/", routes![root])
 }
 
