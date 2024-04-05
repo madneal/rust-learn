f81045c1b3053710917a8d479f00ab1845a10cc2
madneal <bing.ecnu@gmail.com>
2024-04-05 21:27:42 +0800

    initical project

--- src/model.rs
+++
@@ -1,9 +0,0 @@
-use rocket::{FromForm};
-
-#[derive(FromForm, Debug)]
-pub struct Person {
-    #[field(validate=len(1..))]
-    pub(crate) first_name: String,
-    #[field(validate=len(1..))]
-    pub(crate) last_name: String,
-}
